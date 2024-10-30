7f377c5413ccead439c83297b1c7e710c53f840f
Tim Rekelj <hello@timrekelj.si>
2024-10-30 21:10:55 +0100

    Frontend: Get user data from dummyjson API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,47 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const API_URL = 'https://dummyjson.com';
+
+  const [users, setUsers] = useState([]);
+
+  async function request(url, type, params=null) {
+    try {
+      const response = await fetch(API_URL + url)
+
+      if (!response.ok) {
+        throw new Error('API response was not OK');
+      }
+
+      const data = await response.json();
+
+      console.log(data.users)
+
+      setUsers(data.users)
+
+    } catch (error) {
+      console.error('Error fetching data: ', error)
+    }
+  }
+
+  useEffect(() => {
+    request('/users?limit=20', 'GET')
+  }, []); // Empty array ensures this runs once on mount
+
+
   return (
-    <div className="flex items-center h-full w-full">Syyclops Test Assignment</div>
+    <div className="flex items-center h-full w-full">
+      <div className="container mx-auto">
+        <h1 className="text-4xl font-bold text-center">Users</h1>
+        <ul>
+          {users && users.map((user, index) => (
+            <li key={index} className="border p-4 my-2">
+              <p className="font-bold">{user.firstName}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
