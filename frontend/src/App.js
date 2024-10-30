e6e62b56afee715429e3fcb7cf55db16008d6725
Tim Rekelj <hello@timrekelj.si>
2024-10-30 23:39:42 +0100

    Frontend: Update user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,15 +19,34 @@ function App() {
     }));
   };
 
+  const handleUpdate = () => {
+    request(`/users/${selectedUser.id}`, 'PUT', selectedUser)
+  }
+
   const request = async (url, type, params=null) => {
     try {
-      const response = await fetch(API_URL + url);
+      let response;
+      if (type === 'GET') {
+        response = await fetch(API_URL + url);
+      } else if (type === 'PUT') {
+        response = await fetch(API_URL + url, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        });
+      } else {
+        throw new Error('Invalid request type');
+      }
 
       if (!response.ok) {
         throw new Error('API response was not OK');
       }
 
-      return await response.json();
+      let responseJson = await response.json();
+
+      return responseJson;
     } catch (error) {
       console.error('Error fetching data: ', error);
     }
@@ -60,36 +79,36 @@ function App() {
           </ul>
         </div>
         <div className="px-5 col-span-2">
-          <h1 className="text-xl text-center font-bold p-4">{selectedUser.firstName} {selectedUser.lastName}</h1>
+          <h1 className="text-xl text-center font-bold p-4">{selectedUser.firstName || ''} {selectedUser.lastName || ''}</h1>
             <div className="grid grid-cols-4 gap-5 mb-5">
               <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Id:</div>
-              <div className="col-span-3 p-5 border border-blue rounded-lg bg-blue">#{selectedUser.id}</div>
+              <div className="col-span-3 p-5 border border-blue rounded-lg bg-blue">#{selectedUser.id || ''}</div>
             </div>
             <div className="grid grid-cols-4 gap-5">
               <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">First name:</div>
-              <input type="text" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.firstName} autoFocus /><br />
+              <input type="text" name="firstName" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.firstName || '' || ''} autoFocus /><br />
             </div>
             <div className="grid grid-cols-4 gap-5">
               <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Last name:</div>
-            <input type="text" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.lastName} /><br />
+            <input type="text" name="lastName" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.lastName || ''} /><br />
             </div>
             <div className="grid grid-cols-4 gap-5">
               <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Age:</div>
-            <input type="text" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.age} /><br />
+            <input type="text" name="age" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.age || ''} /><br />
             </div>
             <div className="grid grid-cols-4 gap-5">
               <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Gender:</div>
-            <input type="text" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.gender} /><br />
+            <input type="text" name="gender" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.gender || ''} /><br />
             </div>
             <div className="grid grid-cols-4 gap-5">
               <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Email:</div>
-            <input type="text" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.email} /><br />
+            <input type="text" name="email" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.email || ''} /><br />
             </div>
             <div className="grid grid-cols-4 gap-5">
               <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Phone:</div>
-            <input type="text" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.phone} /><br />
+            <input type="text" name="phone" onChange={handleChange} className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value={selectedUser.phone || ''} /><br />
             </div>
-          <button className="bg-orange hover:bg-dark-orange text-white p-5 rounded-lg w-full">Edit user</button>
+          <button className="bg-orange hover:bg-dark-orange text-white p-5 rounded-lg w-full" onClick={handleUpdate}>Edit user</button>
         </div>
       </div>
     </div>
