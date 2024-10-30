59e801f6acfc10195275bde19ee66f75d863ab86
Tim Rekelj <hello@timrekelj.si>
2024-10-30 23:09:57 +0100

    Frontend: Redesign the page, create tailwind configuration and add logo svg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { ReactComponent as Logo} from './images/logo.svg';
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -30,16 +31,54 @@ function App() {
 
 
   return (
-    <div className="flex items-center h-full w-full">
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold text-center">Users</h1>
-        <ul>
-          {users && users.map((user, index) => (
-            <li key={index} className="border p-4 my-2">
-              <p className="font-bold">{user.firstName}</p>
-            </li>
-          ))}
-        </ul>
+    <div className="text-base">
+      <div className="p-10">
+        <Logo />
+      </div>
+
+      <div className="grid grid-cols-3">
+        <div className="mx-5 col-span-1 bg-blue rounded-lg mb-5">
+          <h1 className="text-xl text-center font-bold p-4">Users</h1>
+          <ul>
+            {users && users.map((user, index) => (
+              <li key={index} className="rounded-lg p-3 m-3 hover:bg-light-blue hover:cursor-pointer">
+                <span className="font-bold">#{user.id}</span> {user.firstName} {user.lastName}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="px-5 col-span-2">
+          <h1 className="text-xl text-center font-bold p-4">John Doe</h1>
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Id:</div>
+              <input type="text" className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value="#1" autoFocus/><br />
+            </div>
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">First name:</div>
+              <input type="text" className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value="John"/><br />
+            </div>
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Last name:</div>
+              <input type="text" className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value="Doe"/><br />
+            </div>
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Age:</div>
+              <input type="text" className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value="19"/><br />
+            </div>
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Gender:</div>
+              <input type="text" className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value="Male"/><br />
+            </div>
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Email:</div>
+              <input type="text" className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value="test@mail.com"/><br />
+            </div>
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-1 p-5 rounded-lg border border-blue bg-blue font-bold text-right">Phone:</div>
+              <input type="text" className="col-span-3 p-5 border border-blue rounded-lg bg-light-blue" value="123-456-7890"/><br />
+            </div>
+          <button className="bg-orange hover:bg-dark-orange text-white p-5 rounded-lg w-full">Edit user</button>
+        </div>
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontSize: {
+      base: '1.5rem',
+      xl: '3rem'
+    },
+    colors: {
+      'dark-blue': 'var(--dark-blue)',
+      'blue': 'var(--blue)',
+      'light-blue': 'var(--light-blue)',
+      'orange': 'var(--orange)',
+      'dark-orange': 'var(--dark-orange)',
+    },
     extend: {},
   },
   plugins: [],
 }
-
