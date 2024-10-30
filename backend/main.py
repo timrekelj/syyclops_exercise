3cf4cd5999b711ca45601b4beafbab6af1522cc6
Tim Rekelj <hello@timrekelj.si>
2024-10-31 00:28:16 +0100

    Backend: GET /users endpoint, send users from users.py

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,21 @@ from typing import Union
 from fastapi import FastAPI
 from users import get_users as get_users_from_module
 
+def convert_users(users: list) -> list:
+    users = []
+    for module_user in module_users:
+        user = {}
+        user["id"] = module_user["id"]
+        user["firstName"] = module_user["name"].split(" ")[0]
+        if len(module_user["name"].split(" ")) == 2:
+            user["lastName"] = module_user["name"].split(" ")[1]
+        user["email"] = module_user["email"]
+        users.append(user)
+    return users
+
 app = FastAPI()
-users = get_users_from_module()
+module_users = get_users_from_module()
+users = convert_users(module_users)
 
 @app.get("/")
 def read_root():
@@ -12,5 +25,4 @@ def read_root():
 
 @app.get("/users")
 def get_users():
-    # TODO ASSIGNMENT: Return a list of users
     return users
